639299c567f53d5888d649a0546748b06eb534e1
Riyas mohammed <riyasmohammed475@gmail.com>
2026-06-07 21:35:56 +0530

    AI integration

--- client.py
+++ client.py
@@ -1,9 +1,12 @@
 import os
 from supabase import create_client
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 
 supabase = create_client(
     os.getenv("url"), 
     os.getenv("apikey"))
+
+gemclient = genai.Client(api_key=os.getenv("geminikey"))    
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from client import supabase
 from typing import Optional
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from client import gemclient
+import json
+
 
 app = FastAPI()
 
@@ -13,6 +16,47 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+with open("data.json", "r", encoding="utf-8") as f:
+    profile = json.load(f)
+
+@app.get("/ai")
+class ChatRequest(BaseModel):
+    message: str
+
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+
+    prompt = f"""
+You are Riyas.
+
+Answer as Riyas in first person.
+
+Never say you are an AI model.
+Never mention Gemini or Google.
+
+Use ONLY the information below.
+
+PROFILE:
+{json.dumps(profile, indent=2)}
+
+QUESTION:
+{request.message}
+
+If the answer is not found in the profile, say:
+"I haven't added that information to my portfolio yet."
+"""
+
+    response = gemclient.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    return {
+        "reply": response.text
+    }
+
 
 @app.get("/")
 def give_name():
